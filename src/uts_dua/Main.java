891782b6dd102921e024b066d23cb491a594d163
Mzzqq <rezarifa661@gmail.com>
2023-11-11 23:27:51 +0700

    feat: add exit application

--- src/uts_dua/Main.java
+++ src/uts_dua/Main.java
@@ -16,99 +16,110 @@ public class Main {
         String namaPengguna, status;
         Pengguna pengguna;
 
+        int opsiUtama;
         do {
-            System.out.print("Masukkan nama anda: ");
-            namaPengguna = scanner.nextLine();
+            System.out.println("\nMenu Utama");
+            System.out.println("1. Masuk sebagai User");
+            System.out.println("2. Masuk sebagai Admin");
+            System.out.println("3. Keluar dari Aplikasi");
+            System.out.print("Pilih opsi (1/2/3): ");
+            opsiUtama = scanner.nextInt();
+            scanner.nextLine();  // Membersihkan newline
 
-            System.out.print("Pilih status anda (User/Admin): ");
-            status = scanner.nextLine();
+            switch (opsiUtama) {
+                case 1:
+                    System.out.print("Masukkan nama anda: ");
+                    namaPengguna = scanner.nextLine();
+                    pengguna = new User(namaPengguna);
+                    pengguna.identifikasi();
 
-            if (status.equalsIgnoreCase("User")) {
-                pengguna = new User(namaPengguna);
-            } else if (status.equalsIgnoreCase("Admin")) {
-                pengguna = new Admin(namaPengguna);
-            } else {
-                System.out.println("Status tidak valid");
-                continue;
-            }
+                    User user = (User) pengguna;
+                    int inputUser;
+                    do {
+                        System.out.println("\nMenu Pengguna");
+                        System.out.println("1. Lihat buku");
+                        System.out.println("2. Keluar dari Menu Pengguna");
+                        System.out.print("Pilih opsi (1/2): ");
+                        inputUser = scanner.nextInt();
+                        scanner.nextLine();
 
-            pengguna.identifikasi();
+                        switch (inputUser) {
+                            case 1:
+                                user.lihatBuku(daftarBuku);
+                                break;
+                            case 2:
+                                System.out.println("Keluar dari Menu Pengguna.");
+                                break;
+                            default:
+                                System.out.println("Input tidak valid");
+                        }
+                    } while (inputUser != 2);
+                    break;
 
-            if (pengguna instanceof Admin) {
-                Admin admin = (Admin) pengguna;
-                int menuAdmin;
+                case 2:
+                    System.out.print("Masukkan nama anda: ");
+                    namaPengguna = scanner.nextLine();
+                    pengguna = new Admin(namaPengguna);
+                    pengguna.identifikasi();
 
-                do {
-                    System.out.println("\nMenu Admin");
-                    System.out.println("1. Tambah Buku");
-                    System.out.println("2. Hapus Buku");
-                    System.out.println("3. Lihat Buku");
-                    System.out.println("4. Keluar");
-                    System.out.print("Pilih opsi (1/2/3/4): ");
-                    menuAdmin = scanner.nextInt();
-                    scanner.nextLine();
+                    Admin admin = (Admin) pengguna;
+                    int menuAdmin;
+                    do {
+                        System.out.println("\nMenu Admin");
+                        System.out.println("1. Tambah Buku");
+                        System.out.println("2. Hapus Buku");
+                        System.out.println("3. Lihat Buku");
+                        System.out.println("4. Keluar dari Menu Admin");
+                        System.out.print("Pilih opsi (1/2/3/4): ");
+                        menuAdmin = scanner.nextInt();
+                        scanner.nextLine();
 
-                    switch (menuAdmin) {
-                        case 1:
-                            System.out.print("Masukkan judul buku: ");
-                            String judul = scanner.nextLine();
-                            System.out.print("Masukkan penulis: ");
-                            String penulis = scanner.nextLine();
-                            System.out.print("Masukkan genre: ");
-                            String genre = scanner.nextLine();
-                            admin.tambahBuku(daftarBuku, new Buku(judul, penulis, genre));
-                            break;
-                        case 2:
-                            System.out.print("Masukkan judul yang ingin dihapus: ");
-                            String hapusBuku = scanner.nextLine();
-                            Buku bukuHapus = null;
-                            for (Buku buku: daftarBuku) {
-                                if (buku.getJudul().equalsIgnoreCase(hapusBuku)) {
-                                    bukuHapus = buku;
-                                    break;
+                        switch (menuAdmin) {
+                            case 1:
+                                System.out.print("Masukkan judul buku: ");
+                                String judul = scanner.nextLine();
+                                System.out.print("Masukkan penulis: ");
+                                String penulis = scanner.nextLine();
+                                System.out.print("Masukkan genre: ");
+                                String genre = scanner.nextLine();
+                                admin.tambahBuku(daftarBuku, new Buku(judul, penulis, genre));
+                                break;
+                            case 2:
+                                System.out.print("Masukkan judul yang ingin dihapus: ");
+                                String hapusBuku = scanner.nextLine();
+                                Buku bukuHapus = null;
+                                for (Buku buku: daftarBuku) {
+                                    if (buku.getJudul().equalsIgnoreCase(hapusBuku)) {
+                                        bukuHapus = buku;
+                                        break;
+                                    }
                                 }
-                            }
 
-                            if (bukuHapus != null) {
-                                admin.hapusBuku(daftarBuku, bukuHapus);
-                            } else {
-                                System.out.println("Buku tidak ditemukan");
-                            }
-                            break;
-                        case 3:
-                            Admin.lihatBuku(daftarBuku);
-                            break;
-                        case 4:
-                            System.out.println("Keluar dari Menu Admin.");
-                            break;
-                        default:
-                            System.out.println("Input tidak valid");
-                    }
-                } while (menuAdmin != 4);
-            } else if (pengguna instanceof User) {
-                User user = (User) pengguna;
-                int inputUser;
+                                if (bukuHapus != null) {
+                                    admin.hapusBuku(daftarBuku, bukuHapus);
+                                } else {
+                                    System.out.println("Buku tidak ditemukan");
+                                }
+                                break;
+                            case 3:
+                                Admin.lihatBuku(daftarBuku);
+                                break;
+                            case 4:
+                                System.out.println("Keluar dari Menu Admin.");
+                                break;
+                            default:
+                                System.out.println("Input tidak valid");
+                        }
+                    } while (menuAdmin != 4);
+                    break;
 
-                do {
-                    System.out.println("\nMenu Pengguna");
-                    System.out.println("1. Lihat buku");
-                    System.out.println("2. Keluar");
-                    System.out.print("Pilih opsi (1/2): ");
-                    inputUser = scanner.nextInt();
-                    scanner.nextLine();
+                case 3:
+                    System.out.println("Keluar dari Aplikasi.");
+                    break;
 
-                    switch (inputUser) {
-                        case 1:
-                            user.lihatBuku(daftarBuku);
-                            break;
-                        case 2:
-                            System.out.println("Keluar dari Menu Pengguna.");
-                            break;
-                        default:
-                            System.out.println("Input tidak valid");
-                    }
-                } while (inputUser != 2);
+                default:
+                    System.out.println("Input tidak valid");
             }
-        } while (true);
+        } while (opsiUtama != 3);
     }
 }
