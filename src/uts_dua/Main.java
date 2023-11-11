d3da60cf2893edb19ff0c11a17519ed0fb6fa881
Mzzqq <rezarifa661@gmail.com>
2023-11-11 22:08:20 +0700

    feat: Some changes

--- src/uts_dua/Main.java
+++ src/uts_dua/Main.java
@@ -1,4 +1,25 @@
 package uts_dua;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Buku> daftarBuku = new ArrayList<>();
+        daftarBuku.add(new Buku("Design pattern", "Hawari", "Programming"));
+        daftarBuku.add(new Buku("Domain Driven Development", "Reza", "Tutorial"));
+        daftarBuku.add(new Buku("AC", "Avi", "Language"));
+
+        System.out.print("Masukkan nama anda: ");
+        String namaPengguna = scanner.nextLine();
+
+        System.out.print("Pilih status anda (User/Admin): ");
+        String status = scanner.nextLine();
+
+        Pengguna pengguna;
+
+    }
 }
