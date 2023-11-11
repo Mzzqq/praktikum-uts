1a46066f832c1ba1f4cb7b592aaedcb09d78538b
Mzzqq <rezarifa661@gmail.com>
2023-11-11 21:52:12 +0700

    Initialize commit

--- src/uts_dua/Buku.java
+++ src/uts_dua/Buku.java
@@ -1,4 +1,17 @@
 package uts_dua;
 
-public class Buku {
+class Buku {
+    private String judul;
+    private String penulis;
+    private String genre;
+
+    public Buku(String judul, String penulis, String genre){
+        this.judul = judul;
+        this.penulis = penulis;
+        this.genre = genre;
+    }
+
+    public String toString(){
+        return "Judul: " + judul + ", Penulis: " + penulis + ", genre: " + genre;
+    }
 }
